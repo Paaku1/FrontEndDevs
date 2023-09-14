a8aa89dcdf797306870eeee4e6fa9628b035d946
p1aaku <211001042@rajalakshmi.edu.in>
2023-09-15 00:33:20 +0530

    regex

---
+++ curriculam/bitwiseop.js
@@ -0,0 +1,4 @@
+var a= 5;//5 = 101
+var b = 1;//1 = 001
+var c = a<<b
+console.log(c);
---
+++ curriculam/regex.js
@@ -0,0 +1,4 @@
+let s = `happy birthday maNish thank you welcum`;
+var pattern = /[a-e]/gi;
+var res = s.match(pattern);
+console.log(res);
