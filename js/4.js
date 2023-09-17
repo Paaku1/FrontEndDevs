5b1def228c154564ebea61dcc3e2b3a5d5e6f2cf
p1aaku <211001042@rajalakshmi.edu.in>
2023-09-17 23:27:59 +0530

    bootstrap

--- js/4.js
+++ js/4.js
@@ -1,2 +1,5 @@
-var a = prompt();
-console.log(a);
+async function logMovies() {
+    const response = await fetch("http://example.com/movies.json");
+    const movies = await response.json();
+    console.log(movies);
+}
